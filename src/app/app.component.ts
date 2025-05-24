09b81dcfeed649a63f3f17f6900caf7a1e03c18f
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2025-05-24 23:45:25 +0200

    added base fundament functions(CRUD) and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'essentials';
+  users = DUMMY_USERS;
+  selecedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selecedUserId)!;
+  }
+
+  onselectUser(id: string) {
+    this.selecedUserId = id;
+    console.log('Selected User', id);
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss',
+})
+export class UserComponent {
+  @Input({ required: true }) selected!: boolean;
+  @Input({ required: true }) user!: User;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // select = output<string>();
+  // eslint-disable-next-line @angular-eslint/no-output-native
+  @Output() select = new EventEmitter<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => '/users/' + this.selectedUser().avatar);
+  // imagePath = computed(() => {
+  //   return '/users/' + this.avatar();
+  // });
+  get imagePath() {
+    return '/users/' + this.user.avatar;
+  }
+
+  onselectUser() {
+    this.select.emit(this.user.id);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // console.log('Selected User', this.selectedUser());
+  }
+}
